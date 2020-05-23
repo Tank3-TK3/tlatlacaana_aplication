f0910df9d69741b166f3f66f3d482e6cc931cc51
Roberto Cruz Lozano <47866280+xTank3x@users.noreply.github.com>
2020-05-22 20:00:23 -0500

    Update ta_main_class.py
    The dns.resolver module was added, it was decided to receive some parameters instead of having certain attributes, some variables were changed and the inquire_ip() method was added.

--- CODE/ta_main_class.py
+++ CODE/ta_main_class.py
@@ -21,6 +21,7 @@
 import os #Provides tools for using the operating system.
 import platform #Access to the identification data of the computer.
 import socket #Provides access to the socket interface.
+import dns.resolver #DNS toolkit for Python.
 
 ################################################################################
 #                   CLASS
@@ -28,42 +29,72 @@ import socket #Provides access to the socket interface.
 class Scanner():
     operating_system = "" #Computer operating system.
     computer_name = "" #Computer name.
-    ip_range = "" #Ip range to scan.
-    host_ip = "" #Host ip to scan.
     ping_result = False #Ping result.
     trace_result = "" #Trace result.
     ip_scan = {} #Ip scanner result. (Key = str and value = bool)
     host_dns = "" #DNS of the target.
     port_range = "" #Port range.
     port_result = {} #Port scan result.
+    #ip_min = Minimum ip to scan
+    #ip_max = Maximum ip to scan
+    #host_ip = Host ip to scan.
     
     def __init__(self):
         self.operating_system = platform.system()
         self.computer_name = socket.gethostname()
 
-    def test_connection(self):
+    def test_connection(self, host_ip):
         if self.operating_system is "Windows":
-            if os.system("ping -n 1 " + self.host_ip) is 0:
+            if "TTL=" in os.popen("ping -n 1 " + host_ip).read():
                 self.ping_result = True
             else:
                 self.ping_result = False
         else:
-            if os.system("ping -c 1 " + self.host_ip) is 0:
+            if "ttl=" in os.popen("ping -c 1 " + host_ip).read():
                 self.ping_result = True
             else:
                 self.ping_result = False
         return self.ping_result
 
-    def trace_connection(self):
+    def trace_connection(self, host_ip):
         if self.operating_system is "Windows":
-            if self.test_connection() is True:
-                self.trace_result = os.popen("tracert " + self.host_ip).read()
+            if self.test_connection(host_ip) is True:
+                self.trace_result = os.popen("tracert " + host_ip).read()
             else:
                 self.trace_result = ">>>Destination host not accessible<<<"
         else:
-            if  self.test_connection() is True:
-                self.trace_result = os.popen("tracerout " + self.host_ip).read()
+            if  self.test_connection(host_ip) is True:
+                self.trace_result = os.popen("tracerout " + host_ip).read()
             else:
                 self.trace_result = ">>>Destination host not accessible<<<"
         return self.trace_result
+    
+    def inquire_ip(self, ip_min, ip_max):
+        current_ip = ip_min.split(".")
+        ip_max = ip_max.split(".")
+        flag = True
+        while flag:
+            key_ip = ".".join(current_ip)
+            if current_ip == ip_max:
+                self.ip_scan[key_ip] = self.test_connection(key_ip)
+                flag = False
+            else:
+                self.ip_scan[key_ip] = self.test_connection(key_ip)
+                if(int(current_ip[3]) < 254):
+                    current_ip[3] = str(int(current_ip[3])+1)
+                elif(int(current_ip[2]) < 254):
+                    current_ip[2] = str(int(current_ip[2])+1)
+                    current_ip[3] = "1"
+                elif(int(current_ip[1]) < 254):
+                    current_ip[1] = str(int(current_ip[1])+1)
+                    current_ip[2] = "0"
+                    current_ip[3] = "1"
+                elif(int(current_ip[0]) < 254):
+                    current_ip[0] = str(int(current_ip[0])+1)
+                    current_ip[1] = "0"
+                    current_ip[2] = "0"
+                    current_ip[3] = "1"
+                else:
+                    return self.ip_scan
+        return self.ip_scan
 ################################################################################
